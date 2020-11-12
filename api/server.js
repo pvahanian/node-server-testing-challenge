6b4d1b5a52e24bab180d1e950bb2395b6cb4b528
pvahanian <51976194+pvahanian@users.noreply.github.com>
2020-11-12 15:21:51 -0600

    Commit

---
+++ api/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+// const Modelname = require("../hobbits/hobbitsModel.js");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up" });
+});
+
+server.get("/users", (req, res) => {
+});
+
+module.exports = server;
